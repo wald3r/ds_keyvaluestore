0e1065f30bd3a803d445608ad477c555fdfb4413
Daniel Walder <walder.daniel@gmx.at>
2019-11-13 17:20:21 +0200

    2 dbs

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let DB_URI = process.env.MONGODB_URI
 
-if(process.env.NODE_ENV === 'test'){
-    DB_URI = process.env.TEST_MONGODB_URI
+if(process.env.NODE_ENV === 'production1'){
+    DB_URI = process.env.PRO1_MONGODB_URI
+}else{
+    DB_URI = process.env.PRO2_MONGODB_URI
 }
 
 
