e88cb3177bde3d242d666ec9e7e925d0b8910975
Krisztián Ádám <adamkrisz@protonmail.com>
2019-11-13 16:37:47 +0200

    add untested kafka integration

--- backend/models/pair.js
+++ backend/models/pair.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const kafkaThing = require('../utils/kafkaThing')
 
 
 const pairSchema = mongoose.Schema({
@@ -17,5 +18,36 @@ pairSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
+
+pairSchema.post('findByIdAndUpdate', function(doc) {
+  kafkaThing.send({
+    type: 'update',
+    pair: {
+      key: doc.key,
+      value: doc.value
+    }
+  })
+})
+
+pairSchema.post('save', function(doc) {
+  kafkaThing.send({
+    type: 'create',
+    pair: {
+      key: doc.key,
+      value: doc.value
+    }
+  })
+});
+
+pairSchema.post('remove', function(doc) {
+  kafkaThing.send({
+    type: 'remove',
+    pair: {
+      key: doc.key,
+      value: doc.value
+    }
+  })
+});
 
 module.exports = mongoose.model('Pair', pairSchema)
---
+++ backend/utils/kafkaThing.js
@@ -0,0 +1,70 @@
+const { Kafka } = require('kafkajs')
+ 
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092']
+})
+ 
+const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: Math.random().toString(36).substring(7) })
+ 
+const run = async () => {
+  // Producing
+  await producer.connect()
+ 
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'pairs-topic', fromBeginning: true })
+ 
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      process(message.value)
+      // console.log({
+      //   partition,
+      //   offset: message.offset,
+      //   value: message.value.toString(),
+      // })
+    },
+  })
+}
+ 
+run().catch(console.error)
+
+async function process(msg) {
+  if (msg.type == 'create')
+    processCreate(msg.pair)
+  else if (msg.type == 'update')
+    processUpdate(msg.pair)
+  else if (msg.type == 'remove')
+    processRemove(msg.pair)
+
+}
+
+async function processRemove(pair) {
+  // TODO find by key
+    await Pair.findByIdAndRemove(request.params.id)
+}
+
+async function processUpdate(pair) {
+  // TODO find by pair
+    const updatedPair = await Pair.findByIdAndUpdate(request.params.id, pair, {new: true})
+}
+
+async function processCreate(pair) { 
+    pair = new Pair(pair)
+    
+    await pair.save()
+}
+
+
+async function send(data) {
+  await producer.send({
+    topic: 'pairs-topic',
+    messages: [
+      { value: data },
+    ],
+  })
+
+}
+
+module.exports = { send }
