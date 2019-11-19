f547e270d83ca0ae1ea12b00a7033147c163ad6a
Daniel Walder <walder.daniel@gmx.at>
2019-11-19 09:02:46 +0200

    bugfix

--- backend1/models/pair.js
+++ backend1/models/pair.js
@@ -13,4 +13,14 @@ const pairSchema = mongoose.Schema({
 //add automatically timestamps
 pairSchema.plugin(timestamps)
 
+//change object id
+pairSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
 module.exports = mongoose.model('Pair', pairSchema)
